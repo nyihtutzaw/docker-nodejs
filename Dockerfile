769ef594f3224d92e75e1e8346b13877e279e3af
nyihtutzaw <nyihtutzaw.2015@gmail.com>
2023-04-09 22:39:23 +0630

    add phpmyadmin and mysql

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:15
 WORKDIR /app
 COPY package.json .
-RUN npm install
+RUN npm install --only=production
 COPY . ./
 ENV PORT 3000
 EXPOSE $PORT
-CMD ["npm","run","dev"]
+CMD ["node","index.js"]
--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// Set up Sequelize
+const sequelize = new Sequelize('teams', 'root', 'password', {
+  dialect: 'mysql',
+  host: '172.23.0.2',
+  port: 3306,
+});
+
+// Define a model
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// Sync the models with the database
+sequelize.sync().then(() => {
+  console.log('Database synced');
+}).catch((error) => {
+  console.error('Error syncing database:', error);
+});
+
+// Set up routes
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World');
+});
+
+app.get("/teams",async (req,res)=>{
+  const users=await User.findAll();
+  res.json(users);
 })
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
